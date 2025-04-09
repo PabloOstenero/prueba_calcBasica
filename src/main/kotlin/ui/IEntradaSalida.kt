ef6c6fee25d997f609df425d84e2278b6415254c
PabloOstenero <orpablo@iesrafaelalberti.es>
2025-04-09 16:07:31 +0200

    Error con las excepciones corregido

---
+++ src/main/kotlin/ui/IEntradaSalida.kt
@@ -0,0 +1,11 @@
+package ui
+
+interface IEntradaSalida {
+    fun mostrar(msj: String, salto: Boolean = true)
+    fun mostrarError(msj: String, salto: Boolean = true)
+    fun pedirInfo(msj: String = ""): String
+    fun pedirDouble(msj: String = ""): Double?
+    fun pedirEntero(msj: String = ""): Int?
+    fun preguntar(msj: String = "¿Deseas intentarlo de nuevo? (s/n): "): Boolean
+    fun limpiarPantalla(numSaltos: Int = 20)
+}
